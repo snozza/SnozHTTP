33ee3b54fcc4d9ad9fa6cbcdf075811204f6bbb8
snozza <andrewcsnead@gmail.com>
2016-08-29 09:52:35 +0100

    basic imports

--- SnozHTTP.c
+++ SnozHTTP.c
@@ -1,9 +1,13 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <netinet/in.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define TRUE 1
 #define FALSE 0
+#define EXIT_FAILURE 1
 
 int port;
 int daemon = FALSE;
@@ -19,3 +23,18 @@ struct sockaddr_storage connector;
 int current_socket;
 int connecting_socket;
 socklen_t addr_size;
+
+static void daemonize(void) 
+{
+  pid_t pid, sid;
+  
+  /* already a daemon */
+  if (getppid() == 1) return;
+
+  /* fork off the parent process */
+  pid = fork();
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+
+}
+
